59bf4c1e8b804d351e199a35833107f723d04965
bsce25025 <bsce25025@itu.edu.pk>
2026-01-26 19:35:10 +0500

    sensors

---
+++ Code/NERC_RTR_Main.ino/sensors.h
@@ -0,0 +1,50 @@
+#ifndef SENSORS_H
+#define SENSORS_H
+
+#include <QTRSensors.h>
+#include "settings.h"
+
+QTRSensors qtr;
+unsigned int sensorValues[NUM_SENSORS];
+
+void initSensors() {
+    qtr.setTypeRC();
+    qtr.setSensorPins(SensorPins, NUM_SENSORS);
+    qtr.setEmitterPin(EMITTER_PIN);
+}
+
+void calibrateSensors() {
+    Serial.println("Calibrating... Move robot across the line!");
+    // Blink LED to indicate calibration mode
+    pinMode(25, OUTPUT);
+    digitalWrite(25, HIGH);
+
+    for (uint16_t i = 0; i < 200; i++) {
+        qtr.calibrate();
+        delay(10);
+    }
+    
+    digitalWrite(25, LOW);
+    Serial.println("Calibration Complete.");
+}
+
+// Returns position from 0 to 7000 (3500 is center)
+int getLinePosition() {
+    return qtr.readLineBlack(sensorValues);
+}
+
+// Checks if we are at a grid intersection
+// Returns: 0=None, 1=Intersection Detected
+bool checkIntersection() {
+    bool leftWing   = sensorValues[0] > THRESHOLD;
+    bool rightWing  = sensorValues[7] > THRESHOLD;
+    bool center     = sensorValues[3] > THRESHOLD && sensorValues[4] > THRESHOLD;
+
+    // Detect Cross (+) or T-Junction
+    if ((leftWing && rightWing) || (leftWing && center && rightWing)) {
+        return true;
+    }
+    return false;
+}
+
+#endif
